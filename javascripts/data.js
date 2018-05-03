4f299e7aeda0ef9222d9d6a8766db1be83a573b2
Mehret Kulala <merutaye@gmail.com>
2018-05-02 20:54:53 -0500

    printToDom working

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,12 +1,15 @@
 const loadEx = require('./ex');
 const loadLocation = require('./locations');
+const dom = require('./dom');
 
 const whenExLoad = (data) => {
   console.log('data', data);
+  $('#exContainer').append(dom.writeEx(data.ex));
 };
 
 const whenLocationsLoad = (data) => {
   console.log('data', data);
+  $('#locationContainer').append(dom.writeLocation(data.locations));
 };
 
 const errorFunction = (error) => {
