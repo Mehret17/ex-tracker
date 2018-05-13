5d1e3c7b6f40e293fee61d70a292c1f220f8d168
Mehret Kulala <merutaye@gmail.com>
2018-05-13 02:41:07 -0500

    search complete

--- javascripts/data.js
+++ javascripts/data.js
@@ -1,15 +1,17 @@
 const loadEx = require('./ex');
 const loadLocation = require('./locations');
 const dom = require('./dom');
+const allEvents = require ('./events');
 
 const whenExLoad = (data) => {
   console.log('data', data);
   $('#exContainer').append(dom.writeEx(data.ex));
+  allEvents();
 };
 
 const whenLocationsLoad = (data) => {
   console.log('data', data);
-  $('#locationContainer').append(dom.writeLocation(data.locations));
+  $('#locationContainer').html(dom.writeLocation(data.locations));
 };
 
 const errorFunction = (error) => {
--- javascripts/dom.js
+++ javascripts/dom.js
@@ -1,23 +1,23 @@
 const writeEx = (ex) => {
   let domString = '';
-  domString += `<div class ="exData exCard col-md-6 col-md-offset-3">`;
-  domString += `<img src="${ex.image}" alt="">`;
-  domString += `<h3>${ex.name}</h3>`;
-  domString += `<h4>${ex.age}</h4>`;
-  domString += `<p>${ex.flaws}</p>`;
-  domString += `</dvi>`;
+  domString += `<div class ="exData exCard col-md-6 col-md-offset-3 exName">`;
+  domString += `<img class = "exImage" src="${ex.image}" alt="">`;
+  domString += `<h3>Name: ${ex.name}</h3>`;
+  domString += `<h4>Age: ${ex.age}</h4>`;
+  domString += `<p>Flaw: ${ex.flaws}</p>`;
+  domString += `</div>`;
   return domString;
 };
 
 const writeLocation = (locations) => {
   let strang = '';
   locations.forEach((location) => {
-    strang += `<div class ="locationCard col-md-6">`;
+    strang += `<div class ="locationCard  ${location.time.replace(' ','')} col-md-6">`;
     strang += `<img src="${location.image}" alt="">`;
-    strang += `<h3>${location.name}</h3>`;
-    strang += `<h4>${location.address}</h4>`;
-    strang += `<p>${location.time}</p>`;
-    strang += `<div>`;
+    strang += `<h3 class="locationName">${location.name}</h3>`;
+    strang += `<h4 class ="locationAddress">${location.address}</h4>`;
+    strang += `<p> ${location.time}</p>`;
+    strang += `</div>`;
   });
   return strang;
 };
